0cd25a2c65affb4f1c2b5ae4625be1a5f4b98ac7
Thalandor <thalandor1985@hotmail.com>
2018-02-11 20:28:15 +0100

    Add scripts to generate ddbb

--- DutchTreat/Program.cs
+++ DutchTreat/Program.cs
@@ -10,28 +10,26 @@ using Microsoft.Extensions.Logging;
 
 namespace DutchTreat
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(SetupConfiguration)
-                .UseStartup<Startup>()
-                .Build();
-
-        private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
-        {
-            // Removing the default configuration options.
-            builder.Sources.Clear();
-
-            builder.AddJsonFile("config.json", false, true)
-                .AddEnvironmentVariables();
-
-
-        }
+      BuildWebHost(args).Run();
     }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(SetupConfiguration)
+            .UseStartup<Startup>()
+            .Build();
+
+    private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+    {
+      // Removing the default configuration options
+      builder.Sources.Clear();
+
+      builder.AddJsonFile("config.json", false, true)
+             .AddEnvironmentVariables();
+    }
+  }
 }
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -13,51 +13,65 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace DutchTreat
 {
-    public class Startup
+  public class Startup
+  {
+    private readonly IConfiguration _config;
+
+    public Startup(IConfiguration config)
     {
-        private readonly IConfiguration _config;
-
-        public Startup(IConfiguration config)
-        {
-            this._config = config;
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DutchContext>(cfg =>
-            {
-                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
-            });
-
-            services.AddTransient<IMailService, NullMailService>();
-            // Support for real mail service
-
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseMvc(cfg =>
-            {
-                cfg.MapRoute("Default",
-            "{controller}/{action}/{id?}",
-            new { controller = "App", Action = "Index" });
-            });
-
-        }
+      _config = config;
     }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DutchContext>(cfg =>
+      {
+        cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+      });
+
+      services.AddTransient<IMailService, NullMailService>();
+      // Support for real mail service
+
+      services.AddTransient<DutchSeeder>();
+
+      services.AddScoped<IDutchRepository, DutchRepository>();
+
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseMvc(cfg =>
+      {
+        cfg.MapRoute("Default", 
+          "{controller}/{action}/{id?}", 
+          new { controller = "App", Action = "Index" });
+      });
+
+      if (env.IsDevelopment())
+      {
+        // Seed the database
+        using (var scope = app.ApplicationServices.CreateScope())
+        {
+          var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
+          seeder.Seed();
+        }
+      }
+
+    }
+  }
 }
