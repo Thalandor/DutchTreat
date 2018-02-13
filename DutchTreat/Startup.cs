948c2b90ae7d12d7e024107d53999c90d1324c47
Thalandor <thalandor1985@hotmail.com>
2018-02-13 23:29:45 +0100

    Finished creating the api module

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
@@ -10,68 +11,72 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace DutchTreat
 {
-  public class Startup
-  {
-    private readonly IConfiguration _config;
-
-    public Startup(IConfiguration config)
+    public class Startup
     {
-      _config = config;
-    }
+        private readonly IConfiguration _config;
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DutchContext>(cfg =>
-      {
-        cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
-      });
-
-      services.AddTransient<IMailService, NullMailService>();
-      // Support for real mail service
-
-      services.AddTransient<DutchSeeder>();
-
-      services.AddScoped<IDutchRepository, DutchRepository>();
-
-      services.AddMvc();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/error");
-      }
-
-      app.UseStaticFiles();
-
-      app.UseMvc(cfg =>
-      {
-        cfg.MapRoute("Default", 
-          "{controller}/{action}/{id?}", 
-          new { controller = "App", Action = "Index" });
-      });
-
-      if (env.IsDevelopment())
-      {
-        // Seed the database
-        using (var scope = app.ApplicationServices.CreateScope())
+        public Startup(IConfiguration config)
         {
-          var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
-          seeder.Seed();
+            _config = config;
         }
-      }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+            });
+
+            services.AddAutoMapper();
+
+            services.AddTransient<IMailService, NullMailService>();
+            // Support for real mail service
+
+            services.AddTransient<DutchSeeder>();
+
+            services.AddScoped<IDutchRepository, DutchRepository>();
+
+            services.AddMvc()
+                      .AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+            "{controller}/{action}/{id?}",
+            new { controller = "App", Action = "Index" });
+            });
+
+            if (env.IsDevelopment())
+            {
+                // Seed the database
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
+                    seeder.Seed();
+                }
+            }
+
+        }
     }
-  }
 }
