d2bf59d8f6ef40256d92550345127051f81bad5d
Thalandor <thalandor1985@hotmail.com>
2018-03-01 22:32:47 +0100

    Identity finished

--- DutchTreat/ViewModels/OrderViewModel.cs
+++ DutchTreat/ViewModels/OrderViewModel.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace DutchTreat.ViewModels
 {
-    public class OrderViewModel
-    {
-        public int OrderId { get; set; }
-        public DateTime OrderDate { get; set; }
-        [Required]
-        [MinLength(4)]
-        public string OrderNumber { get; set; }
+  public class OrderViewModel
+  {
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+    [Required]
+    [MinLength(4)]
+    public string OrderNumber { get; set; }
 
-        public ICollection<OrderItemViewModel> Items { get; set; }
-    }
+    public ICollection<OrderItemViewModel> Items { get; set; }
+  }
 }
